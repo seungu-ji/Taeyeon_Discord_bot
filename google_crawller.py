98e97e31cca414b47e471115e2719cb027faf2b2
Jiseungwoo <67452897+viriditass@users.noreply.github.com>
2020-11-22 15:10:32 +0900

    Add files via upload

--- google_crawller.py
+++ google_crawller.py
@@ -2,36 +2,33 @@
 
 from urllib.request import urlretrieve
 from urllib.parse import quote_plus    # korean support
-from bs4 import BeautifulSoup as BS    # Essential module
+from bs4 import BeautifulSoup          # Essential module
 from selenium import webdriver         # Google crolling
 
 keyword = input("Input keyword: ")
-i_URL = f'https://www.google.com/search?q={quote_plus(keyword)}&sxsrf=ALeKk00OQamJ34t56QSInnMzwcC5gC344w:1594968011157&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjXs-7t1tPqAhVF7GEKHfM4DqsQ_AUoAXoECBoQAw&biw=1536&bih=754'
+url = f'https://www.google.com/search?q={quote_plus(keyword)}&sxsrf=ALeKk00OQamJ34t56QSInnMzwcC5gC344w:1594968011157&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjXs-7t1tPqAhVF7GEKHfM4DqsQ_AUoAXoECBoQAw&biw=1536&bih=754'
 
 driver= webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver.get(i_URL)
+driver.get(url)
+for i in range(500):
+   driver.execute_script("window.scrollBy(0,10000)")
 
 html = driver.page_source
-soup = BS(html,features="html.parser")
-
+soup = BeautifulSoup(html)
 img = soup.select('img')
+n = 51
+imgurl = []
 
-i_list = []
-count = 51
-
-print("Searching...")
 for i in img:
    try:
-      i_list.append(i.attrs["src"])
+      imgurl.append(i.attrs["src"])
    except KeyError:
-      i_list.append(i.attrs["data-src"])
+      imgurl.append(i.attrs["data-src"])
 
-print("Downloading...")
-for i in i_list:
-   urlretrieve(i,"img/"+keyword+str(count)+".jpg")
-   count+=1
+for i in imgurl:
+   urlretrieve(i, "./img/"+keyword+str(n)+".jpg")
+   n += 1
+   print(imgurl)
 
 driver.close()
-print("FINISH")
+print("finish")
