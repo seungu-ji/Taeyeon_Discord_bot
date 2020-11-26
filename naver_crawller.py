8792e533c251f6a0ab782c272785a3505c2aed5f
Jiseungwoo <67452897+viriditass@users.noreply.github.com>
2020-11-26 22:45:49 +0900

    Add files via upload

--- naver_crawller.py
+++ naver_crawller.py
@@ -1,23 +1,22 @@
-# 네이버 크롤링
+# naver crawling
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from urllib.parse import quote_plus
 
-baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
-plusUrl = '태연' #input('검색 입력:')
-# 한글 검색 자동 변환
-url = baseUrl + quote_plus(plusUrl)
-html = urlopen(url)
-soup = bs(html, "html.parser")
-img = soup.find_all(class_='_img')
-print(img)
+def crawl_naver_image(plusUrl):
+    baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
+    # 한글 검색 자동 변환
+    url = baseUrl + quote_plus(plusUrl)
+    html = urlopen(url)
+    soup = bs(html, "html.parser")
+    img = soup.find_all(class_='_img')
 
-n = 1
-for i in img:
-    imgUrl = i['data-source']
-    with urlopen(imgUrl) as f:
-        with open('./img/' + plusUrl + str(n)+'.jpg','wb') as h: # w - write b - binary
-            img = f.read()
-            h.write(img)
-    n += 1
-print('다운로드 완료')
+    n = 1
+    for i in img:
+        imgUrl = i['data-source']
+        with urlopen(imgUrl) as f:
+            with open('./img/' + plusUrl + str(n)+'.jpg','wb') as h: # w - write b - binary
+                img = f.read()
+                h.write(img)
+        n += 1
+    print('naver img download')
