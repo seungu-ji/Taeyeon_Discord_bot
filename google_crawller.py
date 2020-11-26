9ff50d2ed490c3dd2360a9c9b06697970d38853f
Jiseungwoo <67452897+viriditass@users.noreply.github.com>
2020-11-26 22:46:07 +0900

    Add files via upload

--- google_crawller.py
+++ google_crawller.py
@@ -1,34 +1,32 @@
-# google crawller
-
+# google crawling
 from urllib.request import urlretrieve
 from urllib.parse import quote_plus    # korean support
 from bs4 import BeautifulSoup          # Essential module
 from selenium import webdriver         # Google crolling
 
-keyword = input("Input keyword: ")
-url = f'https://www.google.com/search?q={quote_plus(keyword)}&sxsrf=ALeKk00OQamJ34t56QSInnMzwcC5gC344w:1594968011157&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjXs-7t1tPqAhVF7GEKHfM4DqsQ_AUoAXoECBoQAw&biw=1536&bih=754'
+def crawl_google_image(keyword):
+   url = f'https://www.google.com/search?q={quote_plus(keyword)}&sxsrf=ALeKk00OQamJ34t56QSInnMzwcC5gC344w:1594968011157&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjXs-7t1tPqAhVF7GEKHfM4DqsQ_AUoAXoECBoQAw&biw=1536&bih=754'
 
-driver= webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
-driver.get(url)
-for i in range(500):
-   driver.execute_script("window.scrollBy(0,10000)")
+   driver= webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
+   driver.get(url)
+   for i in range(500):
+      driver.execute_script("window.scrollBy(0,10000)")
 
-html = driver.page_source
-soup = BeautifulSoup(html)
-img = soup.select('img')
-n = 51
-imgurl = []
+   html = driver.page_source
+   soup = BeautifulSoup(html)
+   img = soup.select('img')
+   n = 51
+   imgurl = []
 
-for i in img:
-   try:
-      imgurl.append(i.attrs["src"])
-   except KeyError:
-      imgurl.append(i.attrs["data-src"])
+   for i in img:
+      try:
+         imgurl.append(i.attrs["src"])
+      except KeyError:
+         imgurl.append(i.attrs["data-src"])
 
-for i in imgurl:
-   urlretrieve(i, "./img/"+keyword+str(n)+".jpg")
-   n += 1
-   print(imgurl)
+   for i in imgurl:
+      urlretrieve(i, "./img/"+keyword+str(n)+".jpg")
+      n += 1
 
-driver.close()
-print("finish")
+   driver.close()
+   print("finish")
